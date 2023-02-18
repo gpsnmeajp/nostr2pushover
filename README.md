06c717eee88612167fa21fe511932ea5c3f3c98a
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2023-02-19 04:11:09 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # nostr2pushover
+Nostrのあなた宛のリプライを、スマートフォン等にプッシュ通知するためのクライアントです。  
+適当なサーバー上などで動かしてください。  
+
+公開鍵のみで利用できます。
+
+利用するためには、pushover.netにて利用登録が必要です。(トライアル期間あり)  
+スマートフォンアプリは800円(買い切り)です。  
+1ヶ月あたり1万回までの通知が利用できます。
+
+https://pushover.net/
+
+It is a client for sending push notifications to smartphones of Nostr's reply to you.
+Please run it on a suitable server.
+
+Only public keys are available.
+
+In order to use it, user registration is required at pushover.net. (with trial period)
+The smartphone app is 800 yen (outright purchase).
+Up to 10,000 notifications are available per month.
+
+https://pushover.net/
+
+# 動作環境
++ node v19.2.0
++ Windows 11 (多分Linuxとかでも動くと思います)
+
+# Licence
+MIT Licence
+
+# Dependencies
+
+```
+  "dependencies": {
+    "nostr-tools": "^1.5.0",
+    "pushover-notifications": "^1.2.2",
+    "websocket-polyfill": "^0.0.3"
+  },
+```
---
+++ index.js
@@ -0,0 +1,81 @@
+const NOSTR_RELAYS = ['wss://nos.lol', 'wss://relay.snort.social', 'wss://nostr.fediverse.jp'];
+const NOSTR_PUBLIC_KEY = "******************************";
+
+// https://pushover.net/
+// 1ヶ月あたり1万メッセージまで
+const PUSHOVER_TOKEN = "******************************";
+const PUSHOVER_USER = "******************************";
+
+var nostr = require('nostr-tools');
+var WebSocket = require('websocket-polyfill');
+var Push = require('pushover-notifications')
+var p = new Push({
+    user: PUSHOVER_USER,
+    token: PUSHOVER_TOKEN,
+})
+
+async function getUserName(pool, pubkey) {
+    let meta = await pool.get(NOSTR_RELAYS, {
+        kinds: [0],
+        authors: [pubkey],
+    });
+    if (meta) {
+        let content = JSON.parse(meta.content);
+        if (content.name) {
+            return "@" + content.name;
+        }
+        if (content.display_name) {
+            return content.display_name;
+        }
+        if (content.username) {
+            return content.username;
+        }
+    }
+    return nostr.nip19.npubEncode(pubkey).substring(0,10);
+}
+
+async function main(){
+    let eose = false;
+    const pool = new nostr.SimplePool();
+    const yourname = await getUserName(pool, nostr.nip19.decode(NOSTR_PUBLIC_KEY).data);
+    console.log(yourname);
+
+    let sub = pool.sub(NOSTR_RELAYS, [{
+        kinds: [1],
+        '#p': [nostr.nip19.decode(NOSTR_PUBLIC_KEY).data]
+    }]);
+
+    sub.on('event', async event => {
+        //Repostは無視
+        if (event.content === "#[0]") {
+            return;
+        }
+        //EOSEまで無視
+        if (eose === false) {
+            return;
+        }
+        let name = await getUserName(pool, event.pubkey);
+        let content = event.content.replace("#[0]", yourname);
+        console.log(event);
+        console.log(name);
+        console.log(content);
+
+        var msg = {
+            message: (name + ": " + content).substring(0,1000),
+            title: "nostr2pushover"
+        }
+        p.send(msg, function (err, result) {
+            if (err) {
+                throw err
+            }
+
+            console.log(result)
+        })
+    });
+
+    sub.on('eose', () => {
+        eose = true;
+        console.log("EOSE");
+    });
+};
+main();
